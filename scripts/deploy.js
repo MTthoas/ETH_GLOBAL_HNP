9b50b6d5f1483e206f575bd9d95e045a3be75bb0
MtThoas <matthias.pecquery@gmail.com>
2023-11-18 22:53:10 +0300

    abi

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,6 @@
-const { ethers } = require("hardhat");
+const { ethers, artifacts } = require("hardhat");
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
     const TokenTransferor = await ethers.getContractFactory("TokenTransferor");
@@ -11,6 +13,21 @@ async function main() {
     await tokenTransferor.deployed();
 
     console.log("TokenTransferor deployed to:", tokenTransferor.address);
+
+    const contractArtifact = await artifacts.readArtifact("TokenTransferor");
+
+    const data = {
+        Contract: {
+            link: "0xD0daae2231E9CB96b94C8512223533293C3693Bf",
+            router: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+            abi: contractArtifact.abi,
+        }
+    };
+
+    fs.writeFileSync(
+        path.join('./client/src/contracts/', 'TokenTransferorABI.json'), 
+        JSON.stringify(data, null, 2)
+    );
 }
 
 main()
